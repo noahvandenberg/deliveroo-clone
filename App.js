258139aada7f7afca4ce608ac97f1a1e0d1bc978
Noah V <noah@noahvandenberg.com>
2022-07-19 20:39:12 -0700

    create homescreen and code header

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { Text, View } from 'react-native';
 import { TailwindProvider } from 'tailwindcss-react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,41 @@
+import { View, Text, SafeAreaView, Image } from 'react-native';
+import React, { useLayoutEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import {
+  UserIcon,
+  ChevronDownIcon,
+  SearchIcon,
+  AdjustmentsIcon,
+} from 'react-native-heroicons/outline';
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  return (
+    <SafeAreaView className='bg-white pt-5'>
+      {/* Header */}
+      <View className='flex-row pb-3 items-center mx-4 space-x-2'>
+        <Image
+          source={{ uri: 'https://links.papareact.com/wru' }}
+          className='h-7 w-7 bg-gray-300 -4 rounded-full'
+        />
+        <View className='flex-1'>
+          <Text className='font-bold text-gray-400 text-xs'>Deliver Now!</Text>
+          <Text className='font-bold text-lg'>
+            Current Location
+            <ChevronDownIcon size={20} color='#00CCBB'></ChevronDownIcon>
+          </Text>
+        </View>
+        <UserIcon size={35} color='#00CCBB' />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
