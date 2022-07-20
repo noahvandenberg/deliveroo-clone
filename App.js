e51d77506d9ae3cf8dba39148ce4de0acf7cbff6
Noah V <noah@noahvandenberg.com>
2022-07-19 18:18:36 -0700

    add react navigation

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import { TailwindProvider } from 'tailwindcss-react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <TailwindProvider>
-      <View>
-        <Text className='text-red-600'>
-          Open up App.js to start working on your app!
-        </Text>
-        <StatusBar style='auto' />
-      </View>
-    </TailwindProvider>
+    <NavigationContainer>
+      <TailwindProvider>
+        <Stack.Navigator>
+          <Stack.Screen name='Home' component={HomeScreen} />
+        </Stack.Navigator>
+      </TailwindProvider>
+    </NavigationContainer>
   );
 }
