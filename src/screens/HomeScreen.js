e1c6c95e9bbe2179d92e1df20521eb86986b94ae
Noah V <noah@noahvandenberg.com>
2022-07-19 20:47:25 -0700

    create search bar

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView, Image } from 'react-native';
+import { View, Text, SafeAreaView, Image, TextInput } from 'react-native';
 import React, { useLayoutEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import {
@@ -20,7 +20,7 @@ const HomeScreen = () => {
   return (
     <SafeAreaView className='bg-white pt-5'>
       {/* Header */}
-      <View className='flex-row pb-3 items-center mx-4 space-x-2'>
+      <View className='flex-row pb-3 items-center mx-4 space-x-2 px-4'>
         <Image
           source={{ uri: 'https://links.papareact.com/wru' }}
           className='h-7 w-7 bg-gray-300 -4 rounded-full'
@@ -33,6 +33,17 @@ const HomeScreen = () => {
           </Text>
         </View>
         <UserIcon size={35} color='#00CCBB' />
+      </View>
+      {/* Search */}
+      <View className='flex-row items-center space-x-2 pb-2 mx-4 px-4'>
+        <View className='flex-row flex-1 space-x-2 bg-gray-200 p-3'>
+          <SearchIcon color='gray' size={20} />
+          <TextInput
+            placeholder='Restaurants and cuisines'
+            keyboardType='default'
+          />
+        </View>
+        <AdjustmentsIcon color='#00CCBB' />
       </View>
     </SafeAreaView>
   );
